5ea857ffe64f5910ec32b7dd519e21e48f045e7c
romanum <romanum@users.noreply.github.com>
2014-05-23 17:24:27 -0400

    adding messages to contribute page

--- www/application/views/contribute.php
+++ www/application/views/contribute.php
@@ -1,7 +1,33 @@
 <?php require_once("header.php")?>
 
 <div class="container">
-	<p class="lead">Help out by sending us a pull request on GitHub.</p>
+	<center>
+		<p class="lead">
+			<a href="https://github.com/salutarismedia/gocatholicevents"
+				target="_blank">Write some code</a>
+		</p>
+		<a href="https://github.com/salutarismedia/gocatholicevents"
+			target="_blank"><img
+			src="<?php echo base_url() . "images/github.png"?>"></a><br> <br>
+		<p>We welcome your requests on GitHub!</p>
+		<br> <br>
+
+		<p class="lead">
+			<a href="https://github.com/salutarismedia/gocatholicevents/issues"
+				target="_blank">Request a feature</a>
+		</p>
+		<p>Let us know what we're missing, or suggest a parish!</p>
+
+		<br> <br>
+		<p class="lead">
+			<a href="https://github.com/salutarismedia/gocatholicevents/issues"
+				target="_blank">Report a bug</a>
+		</p>
+		<p>
+			We've done our best, but sometimes they show up.<br>If you've found a
+			bug, let us know so we can fix it.
+		</p>
+	</center>
 </div>
 
 <?php require_once("footer.php")?>
