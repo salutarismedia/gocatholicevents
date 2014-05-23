ef061dafa87397b593fa6dd17cdec5994497d387
romanum <romanum@users.noreply.github.com>
2014-05-23 17:26:39 -0400

    formatting and addition of an email logo

--- www/application/views/contact.php
+++ www/application/views/contact.php
@@ -1,7 +1,12 @@
 <?php require_once("header.php")?>
 
 <div class="container">
-	<p class="lead">E-mail us at support at gocatholicevents dot com</p>
+	<center>
+		<p class="lead">
+			<span class="glyphicon glyphicon-envelope"></span> E-mail us at
+			support at gocatholicevents dot com
+		</p>
+	</center>
 </div>
 
 <?php require_once("footer.php")?>
