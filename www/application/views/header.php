16de7c1b45692956386111979c445a496700d23e
romanum <romanum@users.noreply.github.com>
2014-05-23 17:27:09 -0400

    including custom css file

--- www/application/views/header.php
+++ www/application/views/header.php
@@ -4,6 +4,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Go Catholic Events</title>
+<link href="<?php echo base_url();?>css/gce.css"
+	rel="stylesheet" />
 <link href="<?php echo base_url();?>css/bootstrap.min.css"
 	rel="stylesheet" />
 <script src="<?php echo base_url();?>js/jquery-1.11.0.min.js"></script>
