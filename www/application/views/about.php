c652d3dc5fd2cb93139beb51dc4f1a87251b560b
romanum <romanum@users.noreply.github.com>
2014-05-23 17:26:26 -0400

    adding message to about page

--- www/application/views/about.php
+++ www/application/views/about.php
@@ -2,8 +2,20 @@
 
 <div class="container">
 
-	<p class="lead text-center">This effort is under the patronage of St. Benedict</p>
-	<p class=""></p>
+	<p class="lead text-center">This effort is under the patronage of St.
+		Benedict</p>
+	Go Catholic Events is an open source project that seeks to bring
+	together all of the Catholic events from different parishes into one
+	place. While we can only be as accurate as the parish sites we glean
+	the information from, we strive to always be as up-to-date as they are.
+	The best way to contribute is by sending us a <a
+		href="https://github.com/salutarismedia/gocatholicevents"
+		target="_blank">GitHub</a> pull request. Not into computers? Help out
+	by spreading the word about our little project, or suggesting some new
+	features you think would help on the <a
+		href="https://github.com/salutarismedia/gocatholicevents/issues"
+		target="_blank">issue tracker</a>. Thanks for reading and God love
+	you!
 </div>
 
 <?php require_once("footer.php")?>
