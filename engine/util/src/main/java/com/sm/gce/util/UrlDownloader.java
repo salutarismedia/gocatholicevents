cf14db2988113223601f021359ba6445972b8a29
romanum <romanum@users.noreply.github.com>
2014-05-30 07:52:12 -0400

    added http referrer to url downloader

--- engine/util/src/main/java/com/sm/gce/util/UrlDownloader.java
+++ engine/util/src/main/java/com/sm/gce/util/UrlDownloader.java
@@ -6,14 +6,20 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 import org.apache.commons.io.IOUtils;
 
 public class UrlDownloader extends LoggingObject {
 
-    public String getUrl(String url) throws MalformedURLException, IOException {
-        logger.debug("Downloading url " + url);
-        InputStream inputStream = new URL(url).openStream();
+    public String getUrl(String urlString) throws MalformedURLException,
+            IOException {
+        logger.debug("Downloading url " + urlString);
+        URL url = new URL(urlString);
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("Referer",
+                "http://www.gocatholicevents.com/");
+        InputStream inputStream = connection.getInputStream();
         BufferedReader bufferedReader = new BufferedReader(
                 new InputStreamReader(inputStream));
         String html = IOUtils.toString(bufferedReader);
