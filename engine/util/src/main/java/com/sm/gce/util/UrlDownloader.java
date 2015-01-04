3a149d72c8e9a404fb5b1cefd73f2e7ccb89e54e
romanum <romanum@users.noreply.github.com>
2015-01-03 23:23:36 -0500

    added http user agent

--- engine/util/src/main/java/com/sm/gce/util/UrlDownloader.java
+++ engine/util/src/main/java/com/sm/gce/util/UrlDownloader.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 import org.apache.commons.io.IOUtils;
 
@@ -16,9 +16,11 @@ public class UrlDownloader extends LoggingObject {
             IOException {
         logger.debug("Downloading url " + urlString);
         URL url = new URL(urlString);
-        URLConnection connection = url.openConnection();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestProperty("Referer",
-                "http://www.gocatholicevents.com/");
+                "http://www.gocatholicevents.com");
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
         InputStream inputStream = connection.getInputStream();
         BufferedReader bufferedReader = new BufferedReader(
                 new InputStreamReader(inputStream));
